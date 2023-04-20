ee87cfbb5db54d77475c19ed8d5eb09b65e93b2b
ldelafue <65057534+ldelafue@users.noreply.github.com>
2023-04-20 11:45:53 -0700

    preprint link

--- Codes/Hydro_LSTM.py
+++ Codes/Hydro_LSTM.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec 17 12:18:48 2021
+By using the Code or the HydroLSTM representation in your publication(s), you agree to cite:
 
-@author: Newpc
+De la Fuente, L. A., Ehsani, M. R., Gupta, H. V., and Condon, L. E.: 
+Towards Interpretable LSTM-based Modelling of Hydrological Systems,
+EGUsphere [preprint], https://doi.org/10.5194/egusphere-2023-666, 2023.
+
 """
 import torch
 import torch.nn as nn
